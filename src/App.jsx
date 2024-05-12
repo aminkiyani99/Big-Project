ff2e67c0365e7c2916e08269e00d2234dc48f5b4
Amin Kiyani <m.zobedei@gmail.com>
2024-05-12 19:58:23 +0330

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
 import second from "./Components/Footer/Footer";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 export default function App() {
   return (
     <div>
@@ -15,9 +18,19 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="man" />} />
-          <Route path="/womens" element={<ShopCategory />} category="woman" />
-          <Route path="/kids" element={<ShopCategory />} category="kid" />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
           <Route path="product" element={<ProductPage />} />
           <Route path=":productId" element={<ProductPage />} />
           <Route path="/cart" element={<Cart />} />
--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import "./CSS/LoginSignUp.css";
 const LoginSignUp = () => {
-  return <div>LoginSignUp</div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder=" Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already Have An Account?<span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By Continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignUp;
